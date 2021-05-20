95b8b500b87ec497c51a414df5fe4b63119d0605
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-05-20 07:15:45 +0530

    Add:Program to insert new contact in addressbbok with database connectivity added

--- src/com/database/AddressBook.sql
+++ src/com/database/AddressBook.sql
@@ -4,3 +4,5 @@ use addressbook_service;
 create table addressbook (FirstName varchar(10) not null, LastName varchar(10) not null, 
 Address varchar(45) not null, City varchar(10) not null, State varchar(10) not null, 
 Zip varchar(10) not null, PhoneNo varchar(15) not null, Email_ID char(50) not null);
+INSERT INTO addressbook(FirstName, LastName, Address, City, State, Zip, PhoneNo, Email_ID)
+ VALUES ('Mayur', 'Pawar', 'Pooja Complex', 'Pen', 'Maharashtra', '402107', '9999999999', 'mayur02@gmail.com');
