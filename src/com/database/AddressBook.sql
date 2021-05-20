5d0e05cfa4d4fb0dd2930c01f127d7e834e5a4c8
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-05-20 07:01:02 +0530

    Add:Addressbook table created with sql client

--- src/com/database/AddressBook.sql
+++ src/com/database/AddressBook.sql
@@ -3,4 +3,4 @@ create database AddressBook_Service;
 use addressbook_service;
 create table addressbook (FirstName varchar(10) not null, LastName varchar(10) not null, 
 Address varchar(45) not null, City varchar(10) not null, State varchar(10) not null, 
-Zip varchar(10) not null, PhoneNo varchar(15) not null, Email_ID char(50) not null) 
+Zip varchar(10) not null, PhoneNo varchar(15) not null, Email_ID char(50) not null);
