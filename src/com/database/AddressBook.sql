7a1260e9f8b4e62d737ee01c4c6f7ff10371a705
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-05-21 06:34:34 +0530

    Add:Query to retrieve contact by city name added

--- src/com/database/AddressBook.sql
+++ src/com/database/AddressBook.sql
@@ -1,9 +1,29 @@
 show databases;
+
+#creating database addressbook Service
 create database AddressBook_Service;
+
+#using database
 use addressbook_service;
+
+#creating table
 create table addressbook (FirstName varchar(10) not null, LastName varchar(10) not null, 
 Address varchar(45) not null, City varchar(10) not null, State varchar(10) not null, 
 Zip varchar(10) not null, PhoneNo varchar(15) not null, Email_ID char(50) not null);
 INSERT INTO addressbook(FirstName, LastName, Address, City, State, Zip, PhoneNo, Email_ID)
  VALUES ('Mayur', 'Pawar', 'Pooja Complex', 'Pen', 'Maharashtra', '402107', '9999999999', 'mayur02@gmail.com');
+
+#deleting contact in addressbook by name
  delete from addressbook where FirstName = 'Mayur';
+ 
+ #inserting data into addressbook
+ INSERT INTO addressbook(FirstName, LastName, Address, City, State, Zip, PhoneNo, Email_ID)
+ VALUES ('Nishad', 'Vairagi', 'Chawdi naka', 'Pen', 'Maharashtra', '402107', '9898989898', 'nishad@gmail.com'),
+ ('Sagar', 'Pawar', 'Kharghar', 'Mumbai', 'Maharashtra', '4011002', '9876543212', 'sagar@gmail.com');
+ 
+ #showing contacts by city name
+ select * from addressbook where City = 'Pen';
+ 
+  #showing contacts by state name
+ select * from addressbook where State = 'Maharashtra';
+ 
